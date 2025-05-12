e3a11e9a8bae4b68530629887e215f83839e4548
helenzel <helenzelalem044@gmail.com>
2025-05-12 16:01:44 +0100

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react" 
+import Home from "./Pages/Home/Home"
+import  "./App.css"
 function App() {
   return (
     <>
-    <h1>hello</h1>
+    <Home/>
     </>
   )
 }
---
+++ netflix-clone/src/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import logo from "../../assets/images/logo/Netflix_Logo_PMS.png"
+function Header() {
+  return (
+    <div className="header_outer_container">
+      <div className="header_container">
+        <div className="header_left">
+           <img src={logo} alt="netflix logo"  />
+          <ul className="nav_links">
+            <li>Netflix</li>
+            <li>Home</li>
+            <li>TVShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className="header_right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
