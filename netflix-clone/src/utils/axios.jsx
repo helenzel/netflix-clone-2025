58ba1775d5e2ca382e7718aa149fb6110982ee25
helenzel <helenzelalem044@gmail.com>
2025-05-13 17:12:26 +0100

    Update axios and requests files, and add Banner component with CSS

--- netflix-clone/src/utils/axios.jsx
+++ netflix-clone/src/utils/axios.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const axios = axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
 });
-export default axios;
+export default axiosInstance;
