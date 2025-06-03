eb47fe353ec25a40df9ab48863ab965be6cdf706
helenzel <helenzelalem044@gmail.com>
2025-06-03 21:16:07 +0100

    base in vite.config

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({mode})=> {
-  return{
+  return {
     plugins: [react()],
-    base:"/netflix-clone-2025/",
+    base: "/netflix-clone-2025/",
   };
 });
